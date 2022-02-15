470eac431d01d33467e5a84e2ef5f3e480ebcbcb
Sofiagonzalezl <92395715+Sofiagonzalezl@users.noreply.github.com>
2022-02-15 15:25:08 -0500

    Add files via upload

---
+++ Array/Array.js
@@ -0,0 +1,8 @@
+edades = []
+nombres = ["Nicolas","Ana","Daniela","Daniel","Gabriela","Sergio","Mariana","Juana","Sofia","Tomas","Samuel","Jorge","Mateo","Jacobo"]
+for(let i = 0; i< 14;i=i+1)
+{
+    edades.push(Number(prompt(`Ingrese la edad de ${i+1}`)))
+}
+
+Promedio =(edadNicolas + edadAna +edadDaniela + edadDaniel + edadGabriela + edadSergio + edadMariana + edadJuana + edadSofia +edadTomas + edadSamuel + edadJorge + edadMateo + edadJacobo )
